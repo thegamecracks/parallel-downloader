75076ef4d9479c0c000172581527320b3467167d
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2024-02-26 19:38:47 -0500

    feat: wrap download entries in scrollable frame

--- pdl.py
+++ pdl.py
@@ -15,9 +15,10 @@ import threading
 from dataclasses import dataclass, field
 from io import BytesIO
 from pathlib import Path, PurePosixPath
-from tkinter import StringVar, Tk
-from tkinter.ttk import Button, Entry, Frame, Progressbar
+from tkinter import Canvas, Event, StringVar, Tk, Widget
+from tkinter.ttk import Button, Entry, Frame, Progressbar, Scrollbar, Style
 from typing import Any, BinaryIO, Protocol, Self
+from weakref import WeakSet
 
 import httpx
 
@@ -96,7 +97,7 @@ class TkDownloadFrame(Frame):
         self.grid_rowconfigure(0, weight=1)
 
         self.list = TkDownloadList(self)
-        self.list.grid(row=0, column=0, sticky="new")
+        self.list.grid(row=0, column=0, sticky="nesw")
         self.total_progress = Progressbar(self)
         self.total_progress.grid(row=1, column=0, sticky="ew")
         self.controls = TkDownloadControls(self)
@@ -126,16 +127,108 @@ class TkDownloadFrame(Frame):
             self.total_progress.step()
 
 
-class TkDownloadList(Frame):
+# https://gist.github.com/thegamecracks/ee5614aa932c2167918a3c3dcc013710
+class ScrollableFrame(Frame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self._canvas = Canvas(self, highlightthickness=0)
+        self._canvas.grid(row=0, column=0, sticky="nesw")
+
+        self._xscrollbar = Scrollbar(self, orient="horizontal")
+        self._yscrollbar = Scrollbar(self, orient="vertical")
+
+        # Use rowspan=2 or columnspan=2 appropriately if filling the
+        # bottom-right corner of the frame is desired.
+        self._xscrollbar.grid(row=1, column=0, sticky="ew")
+        self._yscrollbar.grid(row=0, column=1, sticky="ns")
+
+        self._xscrollbar["command"] = self._canvas.xview
+        self._yscrollbar["command"] = self._canvas.yview
+        self._canvas["xscrollcommand"] = self._wrap_scrollbar_set(self._xscrollbar)
+        self._canvas["yscrollcommand"] = self._wrap_scrollbar_set(self._yscrollbar)
+
+        self.inner = Frame(self._canvas)
+        self._inner_id = self._canvas.create_window(
+            (0, 0), window=self.inner, anchor="nw"
+        )
+
+        self._canvas.bind("<Configure>", lambda event: self._update())
+        self.inner.bind("<Configure>", self._on_inner_configure)
+
+        self._scrolled_widgets = WeakSet()
+        self._style = Style(self)
+        self._update_rate = 125
+        self._update_loop()
+
+    def _on_inner_configure(self, event: Event):
+        background = self._style.lookup(self.inner.winfo_class(), "background")
+        self._canvas.configure(background=background)
+        self._update()
+
+    def _update_loop(self):
+        # Without this, any changes to the inner frame won't affect
+        # the scroll bar/region until the window is resized.
+        self._update()
+        self.after(self._update_rate, self._update_loop)
+
+    def _update(self):
+        # self._canvas.bbox("all") doesn't update until window resize
+        # so we're relying on the inner frame's requested height instead.
+        new_width = max(self._canvas.winfo_width(), self.inner.winfo_reqwidth())
+        new_height = max(self._canvas.winfo_height(), self.inner.winfo_reqheight())
+        bbox = (0, 0, new_width, new_height)
+        self._canvas.configure(scrollregion=bbox)
+        self._canvas.itemconfigure(self._inner_id, width=new_width, height=new_height)
+
+        self._update_scrollbar_visibility(self._xscrollbar)
+        self._update_scrollbar_visibility(self._yscrollbar)
+        self._propagate_scroll_binds(self.inner)
+
+    def _propagate_scroll_binds(self, parent: Widget):
+        if parent not in self._scrolled_widgets:
+            parent.bind("<MouseWheel>", self._on_mouse_yscroll)
+            parent.bind("<Shift-MouseWheel>", self._on_mouse_xscroll)
+            self._scrolled_widgets.add(parent)
+
+        for child in parent.winfo_children():
+            self._propagate_scroll_binds(child)
+
+    def _on_mouse_xscroll(self, event: Event):
+        delta = int(-event.delta / 100)
+        self._canvas.xview_scroll(delta, "units")
+
+    def _on_mouse_yscroll(self, event: Event):
+        delta = int(-event.delta / 100)
+        self._canvas.yview_scroll(delta, "units")
+
+    def _update_scrollbar_visibility(self, scrollbar: Scrollbar):
+        if scrollbar.get() == (0, 1):
+            scrollbar.grid_remove()
+        else:
+            scrollbar.grid()
+
+    def _wrap_scrollbar_set(self, scrollbar: Scrollbar):
+        def wrapper(*args, **kwargs):
+            scrollbar.set(*args, **kwargs)
+            self._update_scrollbar_visibility(scrollbar)
+
+        return wrapper
+
+
+class TkDownloadList(ScrollableFrame):
     def __init__(self, parent: TkDownloadFrame) -> None:
         super().__init__(parent)
 
         self.frame = parent
 
-        self.grid_columnconfigure(0, weight=1)
+        self.inner.grid_columnconfigure(0, weight=1)
 
         self.entries: list[TkDownloadEntry] = []
-        self.new_entry = Button(self, text="+", command=self._do_add_entry)
+        self.new_entry = Button(self.inner, text="+", command=self._do_add_entry)
         self.new_entry.grid(row=999, column=0, sticky="e")
 
     @property
@@ -197,7 +290,7 @@ class TkDownloadList(Frame):
             entry.refresh()
 
     def _do_add_entry(self) -> None:
-        entry = TkDownloadEntry(self)
+        entry = TkDownloadEntry(self.inner, list_=self)
         self.add_entry(entry)
         self.frame.refresh()
 
@@ -206,10 +299,10 @@ class TkDownloadEntry(Frame):
     download: Download | None
     _file: BinaryIO | None
 
-    def __init__(self, parent: TkDownloadList) -> None:
+    def __init__(self, parent: Frame, list_: TkDownloadList) -> None:
         super().__init__(parent)
 
-        self.list = parent
+        self.list = list_
 
         self.grid_columnconfigure(0, weight=1)
 
@@ -223,6 +316,9 @@ class TkDownloadEntry(Frame):
         self.status_entry.state(["disabled"])
 
         self.progress_bar = Progressbar(self)
+
+        for widget in (self.progress_bar, self.status_entry, self.url_entry):
+            widget.grid(row=0, column=0, sticky="ew")
 
         self.remove = Button(self, text="X", command=self._do_remove)
         self.remove.grid(row=0, column=1)
@@ -299,30 +395,30 @@ class TkDownloadEntry(Frame):
 
     def refresh(self) -> None:
         if self.is_running():
-            self.url_entry.grid_forget()
-            self.status_entry.grid_forget()
+            self.url_entry.grid_remove()
+            self.status_entry.grid_remove()
+            self.progress_bar.grid()
 
             self._update_progress()
-            self.progress_bar.grid(row=0, column=0, sticky="ew")
             self.remove.state(["disabled"])
         elif self.status_entry.get() != "":
-            self.url_entry.grid_forget()
-            self.progress_bar.grid_forget()
+            self.url_entry.grid_remove()
+            self.progress_bar.grid_remove()
+            self.status_entry.grid()
 
-            self.status_entry.grid(row=0, column=0, sticky="ew")
             self.remove.state(["!disabled"])
         elif self.has_started():
-            self.status_entry.grid_forget()
-            self.progress_bar.grid_forget()
+            self.status_entry.grid_remove()
+            self.progress_bar.grid_remove()
+            self.url_entry.grid()
 
-            self.url_entry.grid(row=0, column=0, sticky="ew")
             self.url_entry.state(["disabled"])
             self.remove.state(["!disabled"])
         else:
-            self.status_entry.grid_forget()
-            self.progress_bar.grid_forget()
+            self.status_entry.grid_remove()
+            self.progress_bar.grid_remove()
+            self.url_entry.grid()
 
-            self.url_entry.grid(row=0, column=0, sticky="ew")
             self.url_entry.state(["!disabled"])
             self.remove.state(["!disabled"])
 
